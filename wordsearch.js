3dd0bb05cdc6b1e05d24efc7de5fe44999c50506
fathima-thasnim <thasnimfathima492@gmail.com>
2021-12-02 01:58:01 +0000

    word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// const wordSearch = (letters, word) => { 
+//     const horizontalJoin = letters.map(ls => ls.join(''))
+//     for (l of horizontalJoin) {
+//         if (l.includes(word)) return true
+//     }
+// }
 
+// module.exports = wordSearch
+
+const trans = require("./transpose");
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(""))
+    for (l of horizontalJoin) {
+        if (l.includes(word)) {return true}
+    }
+    let newLetters = trans(letters);
+    // console.log(newLetters);
+    const verticalJoin = newLetters.map(ls => ls.join(""));
+    // console.log(verticalJoin);  // correct line
+    for (i of verticalJoin) {
+        if (i.includes(word))
+            {return true}
+        }
+    return false
+}
 module.exports = wordSearch
